9f977150ec3f8fd5405821c26910d476fa09cfca
Madhurjya Majumder <madhurjyamajumder@gmail.com>
2023-05-10 09:07:31 +0530

    final

--- algorithms/sjf.js
+++ algorithms/sjf.js
@@ -1,11 +1,7 @@
 import * as utility from "../utilities/utility.js";
-/*
-  This method aims to calculate the service times for each process queued up for a shortest-job-first scheduling
-*/
+//This method aims to calculate the service times for each process queued up for a shortest-job-first scheduling
 const sjf_CalcTime = (inputTable, sumExecutionTime) => {
-  // Initialize Execution Times Array
   var list_of_execution_times = [];
-
   // Extract each row's priority and execution time
   for (var i = 1; i < inputTable.length; i++) {
     var row = inputTable[i];
@@ -13,7 +9,6 @@ const sjf_CalcTime = (inputTable, sumExecutionTime) => {
       row.children[2].children[0].value
     );
   }
-
   // Determine the next process to schedule based off the current shortest execution time
   var currentIndex = -1;
   for (var i = 0; i < list_of_execution_times.length; i++) {
@@ -26,31 +21,21 @@ const sjf_CalcTime = (inputTable, sumExecutionTime) => {
     sumExecutionTime += list_of_execution_times[currentIndex];
   }
 };
-
-/*
-  This method aims to generate the Ghantts Table for the Shortest Job First Process Scheduling 
-*/
+//This method aims to generate the Ghantts Table for the Shortest Job First Process Scheduling 
 const sjf_Draw = (inputTable, th, td) => {
-  // Initialize List of Execution Times
   var list_of_execution_times = [];
-
   // Extracts Execution Time From Each Process
-
   for (var i = 1; i < inputTable.length; i++) {
     var row = inputTable[i];
     var executeTime = parseInt(row.children[2].children[0].value);
     list_of_execution_times[i - 1] = { executeTime: executeTime, P: i - 1 };
   }
-
   // Sorts list of exection time in ascending order of burst time
-
   list_of_execution_times.sort(function (a, b) {
     if (a.executeTime == b.executeTime) return a.P - b.P;
     return a.executeTime - b.executeTime;
   });
-
   var sumExecutionTime = 0;
-
   // Generates Table Header and Data for Ghantt Chart
   list_of_execution_times.forEach((value, index) => {
     th +=
@@ -62,9 +47,7 @@ const sjf_Draw = (inputTable, th, td) => {
     td += "<td>" + sumExecutionTime + " sec </td>";
     sumExecutionTime += value.executeTime;
   });
-
   td += "<td>" + sumExecutionTime + " sec </td>";
-
   document.querySelector("#timeline").innerHTML =
     '<table id="resultTable" style="width: 70%"><tr>' +
     th +
@@ -75,5 +58,4 @@ const sjf_Draw = (inputTable, th, td) => {
   var timer = document.querySelector("#timer");
   timer.innerHTML = sumExecutionTime + "s";
 };
-
 export { sjf_Draw, sjf_CalcTime };
